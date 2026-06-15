f8a4386b5bb71b322cf05243bc2928b7221138b6
Sanjaydesai21 <sanjaydesai1916@gmail.com>
2026-06-15 11:58:13 +0530

    day 2

---
+++ Day-2/resource.tf
@@ -0,0 +1,83 @@
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
+data "aws_ami" "default_ami" {
+  most_recent = true
+  owners      = var.owners_id
+
+  filter {
+    name   = "ubuntu-ami"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+}
+
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "terraform-key"
+  public_key = tls_private_key.generated.public_key_openssh
+
+  tags = {
+    Name = "terraform-key"
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name   = "ec2_sg"
+  vpc_id = data.aws_vpc.default_vpc.id
+
+  ingress {
+    from_port   = var.ingress_ssh
+    to_port     = var.ingress_ssh
+    protocol    = "tcp"
+    cidr_blocks = [var.ingress_cidr]
+  }
+
+  ingress {
+    from_port   = var.ingress_http
+    to_port     = var.ingress_http
+    protocol    = "tcp"
+    cidr_blocks = [var.ingress_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.ingress_cidr]
+  }
+}
+
+resource "aws_instance" "http_ec2" {
+  ami                    = data.aws_ami.default_ami.id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.my_key.terraform-key
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  user_data              = file("/root/terraform-practice/Day-2/user-data.sh")
+  tags                   = "nginx-ec2"
+}
+
+
+
---
+++ Day-2/variable.tf
@@ -0,0 +1,20 @@
+variable "owners_id" {
+  type    = string
+  default = "318244486837"
+}
+
+variable "ingress_ssh" {
+  default = 22
+}
+
+variable "ingress_http" {
+  default = 80
+}
+
+variable "ingress_cidr" {
+  default = "0.0.0.0/0"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}

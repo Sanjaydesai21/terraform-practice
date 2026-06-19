902dbc1c2b4da7418bec7cce4565ff8e682780c9
Sanjaydesai21 <sanjaydesai1916@gmail.com>
2026-06-19 10:30:30 +0530

    day-8/workspace

--- Day-8/workspace/main.tf
+++ Day-8/workspace/main.tf
@@ -2,6 +2,6 @@ resource "random_id" "new_id" {
   byte_length = 6
 }
 
-resource "aws_s3_bucket" "s3-bucket" {
+resource "aws_s3_bucket" "s3_bucket" {
   bucket = "work_bucket_${terraform.workspace}_${random_id.new_id.hex}"
 }
